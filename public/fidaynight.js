4f001ae0ca293349dcb1863e016a204335dc2871
ThanhLamTr <lamt16482@gmail.com>
2026-03-05 18:13:43 -0800

    update

--- public/fidaynight.js
+++ public/fidaynight.js
@@ -1,4 +1,9 @@
 async function apiSearch(searchTerm){
-    const net="";
+    const net="/.netlify/functions/search";
+    const newfetch= await fetch(net);
+    const hold= await newfetch.json();
+    console.log(hold);
+
     
 }
+apiSearch();
